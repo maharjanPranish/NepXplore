88ca3846e7e53050541b53db0fda3f58a4b899e0
Pranish Maharjan <pranishmaharjan71@gmail.com>
2025-07-23 19:00:44 +0545

    Database Configuration completed and added authController

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const PORT = process.env.PORT;
 const express = require('express');
+const cors = require("cors");
+const dbConfig = require("./config/db.js");
 
 const app = express();
+dbConfig();
+
+
 
 // Middleware to handle CORS
 app.use(cors(
